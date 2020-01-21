a652c31d6a539072f4d1b7d7d09ff081e6b639e4
Matheus Barbosa <matheusbarbosa@medmkt.net>
2020-01-21 16:51:24 -0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'assets/js/*.js',
+        dest: 'build/build.min.js'
+      }
+  },
+  cssmin: {
+      target: {
+          src: ['assets/css/*.css', 'assets/css/*.min.css'],
+          dest: 'build/build.min.css',
+      }
+  },
+  imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'assets/img/',
+                src: ['**/*.{png,jpg,gif,svg}'],
+                dest: 'build/images/'
+            }]
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  grunt.registerTask('run', ['uglify', 'cssmin', 'imagemin']);
+
+};
