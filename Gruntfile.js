0c4ae8c1a9e094469c8d6024a8c551492128f14c
Matheus Barbosa <matheusbarbosa@medmkt.net>
2020-01-21 17:15:21 -0300

    Adicionando bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'assets/js/*.js',
+        src: ['assets/js/jquery-slim.js','assets/js/*.js'],
         dest: 'build/build.min.js'
       }
   },
